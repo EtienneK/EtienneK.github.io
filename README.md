26d8120446f3c83f749053f008531b98e18614f9
Etienne Koekemoer <etiennek@backbase.com>
2020-03-31 10:27:39 +0200

    Fixed security issue

---
+++ README.md
@@ -0,0 +1,11 @@
+# etiennek.com
+
+Build and deploy:
+
+```bash
+cd app
+npm i
+npm run build
+cd ..
+git push origin master
+```
--- app/copy.sh
+++ app/copy.sh
@@ -2,4 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+rm -rf $DIR/../_next
 cp -a $DIR/out/. $DIR/../
